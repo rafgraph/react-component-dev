e5a6bb7b987542d7802c37ae688488971bc80894
rafrex <code@rafrex.com>
2017-05-04 17:30:43 -0700

    Add render function to SomeComponent

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 export default class SomeComponent extends React.Component {
-  return (
-    <div>Hello world from Some Component</div>
-  );
+  render() {
+    return (
+      <div>Hello world from Some Component</div>
+    );
+  }
 }
